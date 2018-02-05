a0dff6dfd1d5ab366fdb1a30b2531c81cd5e5909
Łukasz Rybka <darkstariw@gmail.com>
2018-02-05 10:23:55 +0100

    Second test

--- src/test/java/com/infoshareacademy/jjdd3/FirstJUnitTest.java
+++ src/test/java/com/infoshareacademy/jjdd3/FirstJUnitTest.java
@@ -2,11 +2,17 @@ package com.infoshareacademy.jjdd3;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FirstJUnitTest {
     @Test
     public void firstTest() {
         assertTrue(true);
+    }
+
+    @Test
+    public void secondTest() {
+        assertFalse(false);
     }
 }
